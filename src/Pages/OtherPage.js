afd6f3c360c2648550505247b4441a1d83982f0f
Asep Saepulloh <asep.saepulloh@ITKD000169-MAC.local>
2019-02-21 17:44:12 +0700

    React Hooks

--- src/Pages/OtherPage.js
+++ src/Pages/OtherPage.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import Layout from './Layout'
+import React, { useState, useEffect } from 'react';
+import Layout from './Layout';
 
-export default function OtherPage(){
-    return(
-        <Layout>
-            <h1>OTHER PAGE</h1>
-        </Layout>
-    )
+export default function OtherPage() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Click ${count}`;
+  });
+
+  return (
+    <Layout>
+      <h1>
+        OTHER PAGE
+        {count}
+      </h1>
+      <br />
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Update
+      </button>
+    </Layout>
+  );
 }
