257cd316d36c910fb7d0f15e81552bf1d43dff07
Asep Saepulloh <asep.saepulloh@ITKD000169-MAC.local>
2019-05-10 13:20:26 +0700

    button

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import { withKnobs, text } from '@storybook/addon-knobs';
-import notes from '../notes/button.md';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+import notes from './notes/button.md';
 
 // Component
 import Button from '../src/Components/Button';
@@ -10,9 +10,60 @@ import Button from '../src/Components/Button';
 storiesOf('Component|Button', module)
   .addDecorator(withInfo)
   .addDecorator(withKnobs)
-  .add('Type Success', () => <Button type="success">{text('children', 'Button Success')}</Button>, {
-    notes,
-  })
-  .add('Type Link', () => <Button type="link">{text('children', 'Button Link')}</Button>, {
-    notes,
-  });
+  .add(
+    'Type',
+    () => (
+      <div>
+        <Button type="primary">Button Primary</Button>
+        <Button type="success">Button Success</Button>
+        <Button>Button Default</Button>
+        <Button type="danger">Button Danger</Button>
+        <Button type="dashed">Button Dashed</Button>
+        <Button type="link">Button Link</Button>
+      </div>
+    ),
+    {
+      notes,
+    },
+  )
+  .add(
+    'Disabled',
+    () => (
+      <Button disabled={boolean('disabled', true)} type="success">
+        {text('label', 'Button Disabled')}
+      </Button>
+    ),
+    {
+      notes,
+    },
+  )
+  .add(
+    'Loading',
+    () => (
+      <Button loading={boolean('loading', true)} type="success">
+        {text('label', 'Button Loading')}
+      </Button>
+    ),
+    {
+      notes,
+    },
+  )
+  .add(
+    'Size',
+    () => (
+      <React.Fragment>
+        <Button size="large" type="success">
+          Button Large
+        </Button>
+        <Button size="default" type="success">
+          Button Default
+        </Button>
+        <Button size="small" type="success">
+          Button Small
+        </Button>
+      </React.Fragment>
+    ),
+    {
+      notes,
+    },
+  );
